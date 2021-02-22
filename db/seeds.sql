6a57eb56231b37a46a67aec538a354ac3e9584d7
banuakman <banu@asterya.com>
2021-02-22 16:06:03 -0500

    created schema & seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Spicy Burger');
+INSERT INTO burgers (name) VALUES ('Cheesy Burger');
+INSERT INTO burgers (name) VALUES ('Chipotle Burger');
+INSERT INTO burgers (name) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (name) VALUES ('Barbecue Burger');
+INSERT INTO burgers (name) VALUES ('Meatless Burger');
